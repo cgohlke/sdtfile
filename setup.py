b3f59ce9dae7e0837edfe1166bc8ad143dddb2f5
Christoph Gohlke <cgohlke@uci.edu>
2019-07-03 09:34:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,18 @@ with open('sdtfile/sdtfile.py') as fh:
     code = fh.read()
 
 version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
-description = re.search(r'"""(.*)\.[\r\n?|\n]', code).groups()[0]
-readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}from', code,
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from', code,
                    re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
+
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
                     re.MULTILINE | re.DOTALL).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
@@ -53,5 +57,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        ],
+        'Programming Language :: Python :: 3.8',
+    ],
 )
