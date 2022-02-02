238376b025340d3cd024ef8d8fb5d5b8492838bd
Christoph Gohlke <cgohlke@uci.edu>
2022-02-01 16:22:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
 readme = re.search(
-    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
     code,
     re.MULTILINE | re.DOTALL,
 ).groups()[0]
@@ -55,8 +55,8 @@ setup(
     entry_points={'console_scripts': ['sdt2dat = sdtfile.sdt2dat:main']},
     license='BSD',
     packages=['sdtfile'],
-    python_requires='>=3.7',
-    install_requires=['numpy>=1.15.1'],
+    python_requires='>=3.8',
+    install_requires=['numpy>=1.19.2'],
     platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -65,7 +65,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
